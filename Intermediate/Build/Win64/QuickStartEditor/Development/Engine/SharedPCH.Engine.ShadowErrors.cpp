f61ab20579739d4c7ad7c3a964437933a114eb9e
chiar <chiar@LAPTOP-1PAC5ITU>
2021-10-08 18:42:56 +0200

    Aggiungere i file di progetto.

---
+++ Intermediate/Build/Win64/QuickStartEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30136
+#include "C:/Users/chiar/OneDrive/Documenti/Unreal Projects/QuickStart/Intermediate/Build/Win64/QuickStartEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Source/QuickStart/FloatingActor.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FloatingActor.h"
+#include "Particles/ParticleSystemComponent.h"
+
+// Sets default values
+AFloatingActor::AFloatingActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	VisualMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	VisualMesh->SetupAttachment(RootComponent);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> CubeVisualAsset(TEXT("/Game/StarterContent/Shapes/Shape_Cube.Shape_Cube"));
+
+	if (CubeVisualAsset.Succeeded())
+	{
+		VisualMesh->SetStaticMesh(CubeVisualAsset.Object);
+		VisualMesh->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
+	}
+
+	//
+	OurParticleSystem = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("MovementParticles"));
+	OurParticleSystem->SetupAttachment(VisualMesh);
+	OurParticleSystem->bAutoActivate = true;
+	OurParticleSystem->SetRelativeLocation(FVector(-20.0f, 0.0f, 20.0f));
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> ParticleAsset(TEXT("/Game/StarterContent/Particles/P_Fire.P_Fire"));
+	if (ParticleAsset.Succeeded())
+	{
+		OurParticleSystem->SetTemplate(ParticleAsset.Object);
+	}
+}
+
+
+
+// Called when the game starts or when spawned
+void AFloatingActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFloatingActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector NewLocation = GetActorLocation();
+	FRotator NewRotation = GetActorRotation();
+	float RunningTime = GetGameTimeSinceCreation();
+	float DeltaHeight = (FMath::Sin(RunningTime + DeltaTime) - FMath::Sin(RunningTime));
+	NewLocation.Z += DeltaHeight * FloatSpeed;       //Scale our height by a factor of 20
+	float DeltaRotation = DeltaTime * RotationSpeed;    //Rotate by 20 degrees per second
+	NewRotation.Yaw += DeltaRotation;
+	SetActorLocationAndRotation(NewLocation, NewRotation);
+
+}
+
